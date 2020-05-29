939ac5f45f69f3fca26177697e5e9131ccae038f
David Figatner <david@yopeyopey.com>
2020-05-29 23:17:45 +0800

    published 3.0.4

--- builds/ease.es.js
+++ builds/ease.es.js
@@ -901,12 +901,12 @@ class Easing extends eventemitter3
         if (this.options.wait)
         {
             this.options.wait -= elapsed;
-            if (this.options.wait >= 0)
+            if (this.options.wait > 0)
             {
                 this.emit('wait', this);
                 return
             }
-            if (this.options.wait <= 0)
+            else
             {
                 elapsed = -this.options.wait;
                 this.options.wait = 0;
--- builds/rollup/index.js
+++ builds/rollup/index.js
@@ -39007,12 +39007,12 @@
                     if (this.options.wait)
                     {
                         this.options.wait -= elapsed;
-                        if (this.options.wait >= 0)
+                        if (this.options.wait > 0)
                         {
                             this.emit('wait', this);
                             return
                         }
-                        if (this.options.wait <= 0)
+                        else
                         {
                             elapsed = -this.options.wait;
                             this.options.wait = 0;
