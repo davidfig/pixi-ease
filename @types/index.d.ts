01c54307e971941e7e4b3999971bc506b27fb167
David Figatner <david@yopeyopey.com>
2020-11-27 07:37:59 +0800

    improved types

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -1,8 +1,7 @@
 import * as PIXI from 'pixi.js'
 import { EventEmitter } from 'eventemitter3'
 
-interface EaseOptions
-{
+interface EaseOptions {
     duration?: number
     ease?: string | Function
     useRaf?: boolean
@@ -10,30 +9,28 @@ interface EaseOptions
     maxFrame?: number
 }
 
-interface EaseParams
-{
-     x?: number
-     y?: number
-     position?:  PIXI.DisplayObject | PIXI.Point
-     width?: number
-     height?: number
-     scale?: number
-     scaleX?: number
-     scaleY?: number
-     alpha?: number
-     rotation?: number
-     face?: PIXI.DisplayObject | PIXI.Point
-     skew?: number
-     skewX?: number
-     skewY?: number
-     tint?: number | number[]
-     blend?: number | number[]
-     shake?: number
-     [generic: string]: any
+interface EaseParams {
+    x?: number
+    y?: number
+    position?: PIXI.DisplayObject | PIXI.Point
+    width?: number
+    height?: number
+    scale?: number
+    scaleX?: number
+    scaleY?: number
+    alpha?: number
+    rotation?: number
+    face?: PIXI.DisplayObject | PIXI.Point
+    skew?: number
+    skewX?: number
+    skewY?: number
+    tint?: number | number[]
+    blend?: number | number[]
+    shake?: number
+    [generic: string]: any
 }
 
-interface AddOptions
-{
+interface AddOptions {
     duration?: number
     ease?: string | Function
     repeat?: boolean | number
@@ -44,44 +41,41 @@ interface AddOptions
 
 export let ease: Ease
 
-export declare class Ease extends EventEmitter
-{
+export declare class Ease extends EventEmitter {
     duration: number
     ease: string | Function
 
     constructor(options: EaseOptions)
 
     destroy(): void
-    add(element: PIXI.DisplayObject, params: EaseParams, options: AddOptions): Easing
-    removeAllEases(element: PIXI.DisplayObject): void
+    add(element: PIXI.DisplayObject | PIXI.DisplayObject[], params: EaseParams, options: AddOptions): Easing
+    target(element: PIXI.DisplayObject, target: PIXI.DisplayObject | PIXI.DisplayObject[], speed: number, options: AddOptions): Easing;
+    face(element: PIXI.DisplayObject, target: PIXI.DisplayObject | PIXI.DisplayObject[], speed: number, options: AddOptions): Easing;
     removeEase(element: PIXI.DisplayObject, param?: string | string[]): void
-    removeAll(force: boolean): void
-    update(elapsed?: number): void
+    removeAll(): void
+    update(elapsed: number): void
+    get count(): number;
     countElements(): number
     countRunning(): number
-
-    on(event: 'each' | 'complete', fn: (ease: Ease) => void, context?: any): this
 }
 
-export declare class EaseDisplayObject extends EventEmitter
-{
-    count: number
+export declare class Easing extends EventEmitter {
+    static shortestAngle(start: number, finish: number): number
+    remove(element?: PIXI.DisplayObject, params?: string | string[]): void
 
-    constructor(element: PIXI.DisplayObject, ease: Ease)
-    remove(params: string | string[]): void
-    add(params: EaseParams, options: AddOptions): void
+    constructor(element: PIXI.DisplayObject | PIXI.DisplayObject[], params: EaseParams, options: EaseOptions)
+
+    addParam(element: PIXI.DisplayObject, entry: string, param: any): void
+    updateOne(ease: Ease, entry: string): void
+    updatePoint(ease: Ease, entry: string): void
+    updatePosition(ease: Ease): void
+    updateCoord(ease: Ease, name: string, coord: any): void
+    updateTint(ease: Ease, colors: number[]): void
+    updateBlend(ease: Ease, colors: number[]): void
+    updateShake(ease: Ease): void
+    complete(ease: Ease): void
+    reverse(ease: Ease): void
+    repeat(ease: Ease): void
     update(elapsed: number): void
-}
-
-export declare class Easing extends EventEmitter
-{
-    constructor(element: EaseDisplayObject | EaseDisplayObject[], params: EaseParams, options: EaseOptions)
-
-    addParam(element: EaseDisplayObject, entry: string, param: any): void
-    remove(element: EaseDisplayObject, params?: string | string[]): void
-
-    update(elasped: number): void
     count(): number
-
-    on(event: 'each' | 'complete' | 'repeat' | 'reverse' | 'wait' | 'wait-end', fn: (easing: Easing) => void, context?: any): this
 }
